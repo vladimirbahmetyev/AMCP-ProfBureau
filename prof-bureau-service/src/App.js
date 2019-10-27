ef3af082a918fc177e6f15e03d4b8f2691e6788f
Olga <olga01071999@gmail.com>
2019-10-27 23:11:59 +0300

    add sth

--- prof-bureau-service/src/App.js
+++ prof-bureau-service/src/App.js
@@ -7,8 +7,19 @@ function App() {
         <header className='header'>
             <p className='title'>Профбюро ПМ-ПУ</p>
         </header>
-        <div className='contentBlock'>
-            
+        <div className='mainBlock'>
+            <div className='nav-block'>
+                <ul className="comissionList">
+                  <li className='listElement'>Comission 1</li>
+                  <li className='listElement'>Comissjdfjgdfjjsikdion 2</li>
+                  <li className='listElement'>Comission 3</li>
+                  <li className='listElement'>Comission 4</li>
+                  <li className='listElement'>Comission 5</li>
+                </ul>
+            </div>
+            <div className='content'>
+
+            </div>
         </div>
     </div>
   );
