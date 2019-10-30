7926d8bb4465d1769278053fc72ca94cd5c83c9e
Vladimir <bahv-240298@mail.ru>
2019-10-30 17:45:15 +0300

    Create some bugs

--- prof-bureau-service/src/Personal Acc/CurrentTask.js
+++ prof-bureau-service/src/Personal Acc/CurrentTask.js
@@ -7,13 +7,26 @@ export default class CurrentTask extends React.Component{
     }
 
     state = {
-        exampleState: true,
-        secondExampleState: 0
+        descriptionState:""
     }
+
+    onDescpButtonClick = ()=>{
+        alert("state had been changed")
+        if(this.state.descriptionState ===".current-task-description-hide")
+            this.setState({
+                descriptionState:".current-task-description-show"
+            })        
+        else
+            this.setState({
+                descriptionState:".current-task-description-hide"
+            })
+    }
+
     render(){
     return(
         <div className="current-task-item">
-            <div className="current-task-left"></div>
+        <div className="current-task-item-prev">
+            <div className="current-task-left active-watch-icon"></div>
             <div className="current-task-right">
                 <div className="current-task-name">
                     Название задания
@@ -24,6 +37,23 @@ export default class CurrentTask extends React.Component{
                 </div>
             </div>
         </div>
-    )
-}
+        <div 
+        onClick={() => {
+            alert("state had been changed")
+            if(this.state.descriptionState ===".current-task-description-hide")
+                this.setState({
+                    descriptionState:".current-task-description-show"
+                })        
+            else
+                this.setState({
+                    descriptionState:".current-task-description-hide"
+                })
+        }
+        } 
+        className={this.state.descriptionState}>
+                описание какого то задания бла бла бла авпвапывпвпвыаыа
+            </div>
+        </div>
+        )
+    }
 }
