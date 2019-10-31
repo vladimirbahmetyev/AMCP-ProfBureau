234ee4c1d5d8a0e33ac51862d9451b4fe098f05b
Vladimir <vladimir240298perm@gmail.com>
2019-11-01 00:10:50 +0300

    Done some work

--- prof-bureau-service/src/Personal Acc/ComissionsAndActionsTable/ComissionsTable.js
+++ prof-bureau-service/src/Personal Acc/ComissionsAndActionsTable/ComissionsTable.js
@@ -12,15 +12,45 @@ export default class ComissionsTable extends React.Component{
         <div className="comissions-coloumn">
         <h2 className="com-title">Мои комиссии</h2>
         <div className="comissions-grid">
-            <div className="pm-design com-item"></div>
-            <div className="hr com-item"></div>
-            <div className="pm-profi com-item com-not-active"></div>
-            <div className=" pm-photo com-item com-not-active"></div>
-            <div className="funcom com-item com-not-active"></div>
-            <div className="sportcom com-item com-not-active"></div>
-            <div className="mounting com-item com-not-active"></div>
-            <div className="cultmass com-item"></div>
-            {/* <div className="smi com-item com-not-active"></div> */}
+            <div className="pm-design com-item" onClick={()=>this.props.onClickCom("pmDesign")}>
+                <div className="com-picture"></div>
+                <div>PM-Design</div>
+            </div>
+
+            <div className="hr com-item" onClick={()=>this.props.onClickCom("pmHr")}>
+                <div className="com-picture"></div>
+                <div>HR</div>
+            </div>
+
+            <div className="pm-profi com-item com-not-active" onClick={this.props.onClickCom}>
+                <div className="com-picture"></div>
+                <div>PM-profi</div>
+            </div>
+
+            <div className=" pm-photo com-item com-not-active" onClick={this.props.onClickCom}>
+                <div className="com-picture"></div>
+                <div>PM-photo</div>
+            </div>
+
+            <div className="funcom com-item com-not-active" onClick={this.props.onClickCom}>
+                <div className="com-picture"></div>
+                <div>FunCom</div>
+            </div>
+
+            <div className="sportcom com-item com-not-active" onClick={this.props.onClickCom}>
+                <div className="com-picture"></div>
+                <div>СпортКом</div>
+            </div>
+
+            <div className="mounting com-item com-not-active" onClick={this.props.onClickCom}>
+                <div className="com-picture"></div>
+                <div>Оформители</div>
+            </div>
+
+            <div className="cultmass com-item" onClick={()=>this.props.onClickCom("none")}>
+                <div className="com-picture"></div>
+                <div>КультМасс</div>
+            </div>
         </div>
         </div>
     )
--- prof-bureau-service/src/Personal Acc/PersonalAcc.js
+++ prof-bureau-service/src/Personal Acc/PersonalAcc.js
@@ -3,28 +3,38 @@ import './PersonalAcc.css'
 import HeaderPB from "./HeaderPB"
 import ComissionsTable from "./ComissionsAndActionsTable/ComissionsTable"
 import ActionsTable from "./ComissionsAndActionsTable/ActionsTable"
+// import CurrentComissionEvents from "./CurrentComissionEvents/CurrentComissionsEvents";
 
 import ItemList from "./ItemList/ItemList"
 
+// import CurrentComissionTask from "./CurrentComissionTask/CurrentComissionTask"
 import EndedTask from "./EndedTask/EndedTask"
 import CurrentTask from "./CurrentTask/CurrentTask"
 
 export default class PersonalAccount extends React.Component{
 
     state = {
-        exampleState: true,
-        secondExampleState: 0
+        isAPred: false,
+        selectComission:"none"
+    }
+
+    comTableListener = (pushedCom)=>{
+        this.setState({
+            isAPred:false,
+            selectComission: pushedCom
+        })
     }
     render(){
     return(
     <div>
     <HeaderPB />
     <section className="comissions-and-actions">
-        <ComissionsTable/>
+        <ComissionsTable onClickCom={this.comTableListener}/>
         <ActionsTable/>
     </section>
     <section className="task-status-section">
-        <ItemList type={<CurrentTask/>} titleName = "Мой швапс"/>
+        <ItemList type={<CurrentTask/>} titleName = "Актуальный швапс"/>
+        {/* <CurrentComissionEvents titleName="Швапс комиссии   "/> */}
         <ItemList type={<EndedTask/>} titleName = "Выпитый швапс"/>
     </section>
     </div>)
