770b1afee58b60fe06f067941f7093901097370d
Vladimir <vladimir240298perm@gmail.com>
2019-11-01 17:58:46 +0300

    Add switcher

---
+++ prof-bureau-service/src/Personal Acc/AboutComPred/AboutComPred.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./AboutComPred.css"
+
+export default class AboutComPred extends React.Component{
+
+    render(){
+    return(
+        <div className="about-container">
+            <h2 className="about-com-title">PM-Design</h2>
+            <div>Какое то описание комиссии</div>
+            <div className="about-footer">Председатель комиссии: Крылова Ольга</div>
+        </div>
+    )}}
--- prof-bureau-service/src/Personal Acc/PersonalAcc.js
+++ prof-bureau-service/src/Personal Acc/PersonalAcc.js
@@ -1,13 +1,15 @@
 import  React from "react"
 import './PersonalAcc.css'
 import HeaderPB from "./HeaderPB"
-import ComissionsTable from "./ComissionsAndActionsTable/ComissionsTable"
-import ActionsTable from "./ComissionsAndActionsTable/ActionsTable"
-// import CurrentComissionEvents from "./CurrentComissionEvents/CurrentComissionsEvents";
+import ComissionsTable from "./ComissionsTable/ComissionsTable"
+import ActionsTable from "./ActionsTable/ActionsTable"
+import CurrentComissionEvents from "./CurrentComissionEvents/CurrentComissionsEvents";
+import AboutComPred from "./AboutComPred/AboutComPred";
+import CssTransition from "react-transition-group/CSSTransitionGroup";
 
 import ItemList from "./ItemList/ItemList"
 
-// import CurrentComissionTask from "./CurrentComissionTask/CurrentComissionTask"
+import CurrentComissionTask from "./CurrentComissionTask/CurrentComissionTask"
 import EndedTask from "./EndedTask/EndedTask"
 import CurrentTask from "./CurrentTask/CurrentTask"
 
@@ -15,14 +17,31 @@ export default class PersonalAccount extends React.Component{
 
     state = {
         isAPred: false,
-        selectComission:"none"
+        selectComission:"none",
+        aboutCom: <ActionsTable/>,
+        leftTaskPart: <ItemList type={<CurrentTask/>} titleName = "Актуальный швапс"/>,
+        rightTaskPart:<ItemList type={<EndedTask/>} titleName = "Выпитый швапс"/>
     }
 
     comTableListener = (pushedCom)=>{
-        this.setState({
-            isAPred:false,
-            selectComission: pushedCom
-        })
+        if (pushedCom ==="none") {
+            this.setState({
+                isAPred: false,
+                selectComission:"none",
+                aboutCom: <ActionsTable/>,
+                leftTaskPart: <ItemList type={<CurrentTask/>} titleName = "Мой швапс"/>,
+                rightTaskPart:<ItemList type={<EndedTask/>} titleName = "Выпитый швапс"/>
+            })
+        }
+        else{
+            this.setState({
+                isAPred:false,
+                selectComission: pushedCom,
+                aboutCom: <AboutComPred/>,
+                leftTaskPart: <ItemList type={<CurrentComissionTask/>} titleName="Актуальный швапс"/>,
+                rightTaskPart:<CurrentComissionEvents titleName="Швапс комиссии"/>
+            })
+        }
     }
     render(){
     return(
@@ -30,13 +49,14 @@ export default class PersonalAccount extends React.Component{
     <HeaderPB />
     <section className="comissions-and-actions">
         <ComissionsTable onClickCom={this.comTableListener}/>
-        <ActionsTable/>
+        {this.state.aboutCom}
     </section>
+    <CssTransition appear={true} in={true} timeout={300} classNames="fade">
     <section className="task-status-section">
-        <ItemList type={<CurrentTask/>} titleName = "Актуальный швапс"/>
-        {/* <CurrentComissionEvents titleName="Швапс комиссии   "/> */}
-        <ItemList type={<EndedTask/>} titleName = "Выпитый швапс"/>
+        {this.state.leftTaskPart}        
+        {this.state.rightTaskPart}        
     </section>
+    </CssTransition>
     </div>)
     }
 }
