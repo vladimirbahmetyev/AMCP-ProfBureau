993b6e7424424dcb4d67c897a0818060d25a816d
Vladimir <vladimir240298perm@gmail.com>
2019-11-12 13:09:01 +0300

    Fix JSON after merge

--- prof-bureau-service/src/App.js
+++ prof-bureau-service/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';  
 import MainPage from './MainPage/MainPage';
+import SecondPafe from './Personal Acc/PersonalAcc'
 
 class App extends React.Component {
     render() {
         return (
-            <MainPage />
+            <SecondPafe />
         );
     }
 }
