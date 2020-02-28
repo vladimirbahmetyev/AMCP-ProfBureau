183af3c83a58652e53f1351dbcf489bcaac27c8d
Olga <olyakrylo24@gmail.com>
2020-02-28 16:09:38 +0300

    ugh

---
+++ prof-bureau-service/build/precache-manifest.4f5a4e4a5c41181e245e6348d6c2ead8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8c4552f9befb7f3098af7ec25597e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2612753c00229b57e4e",
+    "url": "/static/css/main.17a53ca9.chunk.css"
+  },
+  {
+    "revision": "89324a68661a4670306c",
+    "url": "/static/js/2.f8275f46.chunk.js"
+  },
+  {
+    "revision": "1715589d805e10ae2d500be9501a1cef",
+    "url": "/static/js/2.f8275f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2612753c00229b57e4e",
+    "url": "/static/js/main.d0945f6f.chunk.js"
+  },
+  {
+    "revision": "88cd832def3a42db2a5b",
+    "url": "/static/js/runtime-main.3cbfaeff.js"
+  },
+  {
+    "revision": "e7f4bcb7f394ad6f2d1d41c8eb17cc56",
+    "url": "/static/media/PFBeauSansPro-Light.e7f4bcb7.ttf"
+  },
+  {
+    "revision": "750b71837ec0f6372a208235f565418a",
+    "url": "/static/media/PFBeauSansPro-LightItal.750b7183.ttf"
+  },
+  {
+    "revision": "07bae36d388e2852cbd5cf5819365899",
+    "url": "/static/media/PFBeauSansPro-SeBold.07bae36d.ttf"
+  },
+  {
+    "revision": "35f1bb0498483b036266f8d56a5ee35a",
+    "url": "/static/media/PM-cropped-blured.35f1bb04.jpg"
+  },
+  {
+    "revision": "e2bec59b65c6d25dc559f4f1bfe36381",
+    "url": "/static/media/cultmass.e2bec59b.jpg"
+  },
+  {
+    "revision": "8adfda0b86336e563699131627de2099",
+    "url": "/static/media/design.8adfda0b.jpg"
+  },
+  {
+    "revision": "43cd9957166734bc22aa128e5390898c",
+    "url": "/static/media/funcom.43cd9957.jpg"
+  },
+  {
+    "revision": "f0dd39f2057d70e9f0da18d0f9d4ce23",
+    "url": "/static/media/hr.f0dd39f2.jpg"
+  },
+  {
+    "revision": "b774d0c11828a33f01e50f1703dce4a3",
+    "url": "/static/media/oformiteli.b774d0c1.jpg"
+  },
+  {
+    "revision": "faf997a0f1c1cec2871cd57e2ff0ffc2",
+    "url": "/static/media/partner.faf997a0.jpg"
+  },
+  {
+    "revision": "ad1e331e3974c233f6e7ca5d977b7f7b",
+    "url": "/static/media/pb.ad1e331e.jpg"
+  },
+  {
+    "revision": "0902c31e05f093e051980de960dbe3f4",
+    "url": "/static/media/photo.0902c31e.jpg"
+  },
+  {
+    "revision": "94052f7c133963637007f9454e546b59",
+    "url": "/static/media/sportcom.94052f7c.jpg"
+  },
+  {
+    "revision": "1f1ebf152da2e7f1f1a2fdc4c0b94fcd",
+    "url": "/static/media/sprite.1f1ebf15.svg"
+  }
+]);
